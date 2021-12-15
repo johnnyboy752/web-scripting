5cc0b0555503cacc60e2c9b969b0d84c5a490af2
johnnyboy752 <john.m.keating4@gmail.com>
2021-12-15 18:47:17 -0500

    commit

---
+++ finalExam.js
@@ -0,0 +1,9 @@
+function conceptOne() {
+    // Changes paragraph using .innerHtml
+    document.getElementById("conceptOne").innerHTML = "WOW! I can change the text in a paragraph using the document.getElementById(id) method.";
+}
+
+function conceptTwo() {
+    //Calculates 100 + 50
+    document.getElementById("conceptTwoAnswer").innerHTML = 100 + 50
+}
--- waterCalculator.js
+++ waterCalculator.js
@@ -23,7 +23,6 @@ function waterCalculator(event)
     currentHour,
     dates,
     greeting,
-    userErrorMessage,
     waterBill,
     waterBillTax,
     totalWaterBill;
@@ -62,28 +61,14 @@ dates += "<mark>" + previousDate + "</mark>" + " to " + "<mark>" + yesterdayDate
     document.getElementById("calculatorResults").style.visibility = "visible";    
     //Handle user-input errors for the initial Reading and Final Reading
     //Display error messages and do not allow the form to process user input
-        if (initialReading < 0)
+        if (initialReading < 0 || (finalReading < 0) || (taxPercent < 0))
         {
-            userErrorMessage = 'The initial reading must be non-negative. Click "Clear" to clear this error message and try again.';
-            document.getElementById("userErrorMessage").innerHTML = userErrorMessage;
-            return;
-        }
-        if (finalReading < 0)
-        {
-            userErrorMessage = 'The final reading must be non-negative. Click "Clear" to clear this error message and try again.';
-            document.getElementById("userErrorMessage").innerHTML = userErrorMessage;
+            alert("Error: \nThe initial reading cannot be less than zero. OR \nThe final reading cannot be less than zero. OR \nThe tax cannot be less than zero.");
             return;
         }
         if (finalReading < initialReading)
         {
-            userErrorMessage = 'Final reading cannot be less then the initial reading. Click "Clear" to clear this error message and try again.';
-            document.getElementById("userErrorMessage").innerHTML=userErrorMessage;
-            return;
-        }
-        if (taxPercent < 0)
-        {
-            userErrorMessage = 'Tax must be a non-negative. Click "Clear" to clear this error message and try again.';
-            document.getElementById("userErrorMessage").innerHTML = userErrorMessage;
+            alert("Error: The final reading cannot be less than the inital reading!");
             return;
         }
 
@@ -95,39 +80,24 @@ dates += "<mark>" + previousDate + "</mark>" + " to " + "<mark>" + yesterdayDate
         waterBill = 2.73 * waterUsage + 4;
         waterBillTax = waterBill * mathTax;
         totalWaterBill = waterBillTax + waterBill;
-        document.getElementById("firstLastNames").innerHTML = "Good " + greeting + " " + fName + " " + lName + ".";
-        document.getElementById("dates").innerHTML = dates;
-        document.getElementById("iReading").innerHTML = "The initial reading was " + initialReading + " hcf.";
-        document.getElementById("fReading").innerHTML = "The final reading, as of yesterday, was " + finalReading + " hcf.";
-        document.getElementById("waterUsage").innerHTML = "Your total water usage for this month was " + (finalReading - initialReading) + " hcf.";
-        document.getElementById("taxRate").innerHTML = "The tax you paid is $" + waterBillTax.toFixed(2) + ".";
-        document.getElementById("displayBillTotal").innerHTML = "Your total water bill for this month is $" + totalWaterBill.toFixed(2) + ".";
-        document.getElementById("thankYou").innerHTML = "Thank you for using this service! Have a wonderful rest of your " + greeting + "!";
     } else if ((waterUsage > 30) && (waterUsage <= 5000)) {
         waterBill = 2.45 * waterUsage + 12.4;
         waterBillTax = waterBill * mathTax;
         totalWaterBill = waterBillTax + waterBill;
-        document.getElementById("firstLastNames").innerHTML = greeting + " " + fName + " " + lName + ".";
-        document.getElementById("dates").innerHTML = dates;
-        document.getElementById("iReading").innerHTML = "The initial reading was " + initialReading + " hcf.";
-        document.getElementById("fReading").innerHTML = "The final reading, as of yesterday, was " + finalReading + " hcf.";
-        document.getElementById("waterUsage").innerHTML = "Your total water usage for this month was " + (finalReading - initialReading) + " hcf.";
-        document.getElementById("taxRate").innerHTML = "The tax you paid is $" + waterBillTax.toFixed(2) + ".";
-        document.getElementById("displayBillTotal").innerHTML = "Your total water bill for this month is $" + totalWaterBill.toFixed(2) + ".";
-        document.getElementById("thankYou").innerHTML = "Thank you for using this service! Have a wonderful rest of your " + greeting + "!";
     } else if (waterUsage > 5000) {
         waterBill = 1.29 * waterUsage + 5812.4;
         waterBillTax = waterBill * mathTax;
         totalWaterBill = waterBillTax + waterBill;
-        document.getElementById("firstLastNames").innerHTML = greeting + " " + fName + " " + lName + ".";
-        document.getElementById("dates").innerHTML = dates;
-        document.getElementById("iReading").innerHTML = "The initial reading was " + initialReading + " hcf.";
-        document.getElementById("fReading").innerHTML = "The final reading, as of yesterday, was " + finalReading + " hcf.";
-        document.getElementById("waterUsage").innerHTML = "Your total water usage for this month was " + (finalReading - initialReading) + " hcf.";
-        document.getElementById("taxRate").innerHTML = "The tax you paid is $" + waterBillTax.toFixed(2) + ".";
-        document.getElementById("displayBillTotal").innerHTML = "Your total water bill for this month is $" + totalWaterBill.toFixed(2) + ".";
-        document.getElementById("thankYou").innerHTML = "Thank you for using this service! Have a wonderful rest of your " + greeting + "!";
     }
+
+    document.getElementById("firstLastNames").innerHTML = "Good " + greeting + " " + fName + " " + lName + ".";
+    document.getElementById("dates").innerHTML = dates;
+    document.getElementById("iReading").innerHTML = "The initial reading was " + initialReading + " hcf.";
+    document.getElementById("fReading").innerHTML = "The final reading, as of yesterday, was " + finalReading + " hcf.";
+    document.getElementById("waterUsage").innerHTML = "Your total water usage for this month was " + (finalReading - initialReading) + " hcf.";
+    document.getElementById("taxRate").innerHTML = "The tax is $" + waterBillTax.toFixed(2) + ".";
+    document.getElementById("displayBillTotal").innerHTML = "Your total water bill for this month is $" + totalWaterBill.toFixed(2) + ".";
+    document.getElementById("thankYou").innerHTML = "Thank you for using this service! Have a wonderful rest of your " + greeting + "!";
 
 }
 
@@ -139,5 +109,4 @@ function erase()
     document.getElementById("finalReading").value = " ";
     document.getElementById("taxPercent").value =  " ";
     document.getElementById("calculatorResults").style.visibility = "hidden";
-    document.getElementById("userErrorMessage").innerHTML = " ";
 }   

f5e3adc938e8093182a3d433d3b132ac409bdcd5
johnnyboy752 <john.m.keating4@gmail.com>
2021-12-15 13:14:03 -0500

    commit

--- waterCalculator.js
+++ waterCalculator.js
@@ -13,17 +13,53 @@ function waterCalculator(event)
     lName = document.getElementById("lName").value,
     initialReading = parseFloat(document.getElementById("initialReading").value, 10),
     finalReading = parseFloat(document.getElementById("finalReading").value, 10),
-    taxPercent = parseFloat(document.getElementById("taxPercent").value, 10);
-    var waterUsage;
-    var totalWaterBill;
-    var mathTax = taxPercent /100;
+    taxPercent = parseFloat(document.getElementById("taxPercent").value, 10),
+    waterUsage,
+    totalWaterBill,
+    mathTax = taxPercent /100,
+    currentDate,
+    yesterdayDate,
+    previousDate,
+    currentHour,
+    dates,
+    greeting,
+    userErrorMessage,
+    waterBill,
+    waterBillTax,
+    totalWaterBill;
 
-    document.getElementById("calculatorResults").style.visibility = "visible";
-           
-    // Variables for Error Messages
-            var userErrorMessage;
+    
+ //Display the date in results div element
+ currentDate = new Date();
+ currentDate.getDate();
+    
+ currentHour = currentDate.getHours();
+    
+ yesterdayDate = new Date();
+ yesterdayDate.setDate(currentDate.getDate() - 1);
+ yesterdayDate = new Date(yesterdayDate).toUTCString();
+ yesterdayDate = yesterdayDate.split(' ').slice(0, 4).join(' ');
+    
+    
+ previousDate = new Date();
+ previousDate.setMonth(previousDate.getMonth() - 1);
+ previousDate = new Date(previousDate).toUTCString();
+ previousDate = previousDate.split(' ').slice(0, 4).join(' ');
+ 
+if (currentHour < 12) {
+    greeting = "morning";
+} else if (currentHour >= 12 && currentHour < 16) {
+    greeting = "afternoon";
+} else if (currentHour >= 16 && currentHour <=24) {
+    greeting = "evening";
+}
 
-            
+dates = "This is your Water Bill Statement printed today: " + currentDate + "." + "<br>";
+dates += "This bill covers the water usage from: ";
+dates += "<mark>" + previousDate + "</mark>" + " to " + "<mark>" + yesterdayDate + "</mark>.";
+
+
+    document.getElementById("calculatorResults").style.visibility = "visible";    
     //Handle user-input errors for the initial Reading and Final Reading
     //Display error messages and do not allow the form to process user input
         if (initialReading < 0)
@@ -54,49 +90,45 @@ function waterCalculator(event)
 
     // Determines the rate depending on how many hcf's of water were used based on the final meter reading - inital meter reading
     waterUsage = finalReading - initialReading;
-    var waterBill;
-    var waterBillTax;
-    var totalWaterBill;
     if ((waterUsage >= 0) && (waterUsage <= 30)) 
     {
         waterBill = 2.73 * waterUsage + 4;
         waterBillTax = waterBill * mathTax;
         totalWaterBill = waterBillTax + waterBill;
-        document.getElementById("billFacts").innerHTML = "This bill covers the last month of water activity.";
+        document.getElementById("firstLastNames").innerHTML = "Good " + greeting + " " + fName + " " + lName + ".";
+        document.getElementById("dates").innerHTML = dates;
         document.getElementById("iReading").innerHTML = "The initial reading was " + initialReading + " hcf.";
-        document.getElementById("fReading").innerHTML = "The final reading was " + finalReading + " hcf.";
+        document.getElementById("fReading").innerHTML = "The final reading, as of yesterday, was " + finalReading + " hcf.";
         document.getElementById("waterUsage").innerHTML = "Your total water usage for this month was " + (finalReading - initialReading) + " hcf.";
-        document.getElementById("taxRate").innerHTML = "The tax rate is " + taxPercent + "%.";
-        document.getElementById("displayBillTotal").innerHTML = "Your total bill for this month is $" + totalWaterBill.toFixed(2) + ".";
+        document.getElementById("taxRate").innerHTML = "The tax you paid is $" + waterBillTax.toFixed(2) + ".";
+        document.getElementById("displayBillTotal").innerHTML = "Your total water bill for this month is $" + totalWaterBill.toFixed(2) + ".";
+        document.getElementById("thankYou").innerHTML = "Thank you for using this service! Have a wonderful rest of your " + greeting + "!";
     } else if ((waterUsage > 30) && (waterUsage <= 5000)) {
         waterBill = 2.45 * waterUsage + 12.4;
         waterBillTax = waterBill * mathTax;
         totalWaterBill = waterBillTax + waterBill;
-        document.getElementById("billFacts").innerHTML = "This bill covers the last month of water activity.";
+        document.getElementById("firstLastNames").innerHTML = greeting + " " + fName + " " + lName + ".";
+        document.getElementById("dates").innerHTML = dates;
         document.getElementById("iReading").innerHTML = "The initial reading was " + initialReading + " hcf.";
-        document.getElementById("fReading").innerHTML = "The final reading was " + finalReading + " hcf.";
+        document.getElementById("fReading").innerHTML = "The final reading, as of yesterday, was " + finalReading + " hcf.";
         document.getElementById("waterUsage").innerHTML = "Your total water usage for this month was " + (finalReading - initialReading) + " hcf.";
-        document.getElementById("taxRate").innerHTML = "The tax rate is " + taxPercent + "%.";
-        document.getElementById("displayBillTotal").innerHTML = "Your total bill for this month is $" + totalWaterBill.toFixed(2) + ".";
+        document.getElementById("taxRate").innerHTML = "The tax you paid is $" + waterBillTax.toFixed(2) + ".";
+        document.getElementById("displayBillTotal").innerHTML = "Your total water bill for this month is $" + totalWaterBill.toFixed(2) + ".";
+        document.getElementById("thankYou").innerHTML = "Thank you for using this service! Have a wonderful rest of your " + greeting + "!";
     } else if (waterUsage > 5000) {
         waterBill = 1.29 * waterUsage + 5812.4;
         waterBillTax = waterBill * mathTax;
         totalWaterBill = waterBillTax + waterBill;
-        document.getElementById("billFacts").innerHTML = "This bill covers the last month of water activity.";
+        document.getElementById("firstLastNames").innerHTML = greeting + " " + fName + " " + lName + ".";
+        document.getElementById("dates").innerHTML = dates;
         document.getElementById("iReading").innerHTML = "The initial reading was " + initialReading + " hcf.";
-        document.getElementById("fReading").innerHTML = "The final reading was " + finalReading + " hcf.";
+        document.getElementById("fReading").innerHTML = "The final reading, as of yesterday, was " + finalReading + " hcf.";
         document.getElementById("waterUsage").innerHTML = "Your total water usage for this month was " + (finalReading - initialReading) + " hcf.";
-        document.getElementById("taxRate").innerHTML = "The tax rate is " + taxPercent + "%.";
-        document.getElementById("displayBillTotal").innerHTML = "Your total bill for this month is $" + totalWaterBill.toFixed(2) + ".";
+        document.getElementById("taxRate").innerHTML = "The tax you paid is $" + waterBillTax.toFixed(2) + ".";
+        document.getElementById("displayBillTotal").innerHTML = "Your total water bill for this month is $" + totalWaterBill.toFixed(2) + ".";
+        document.getElementById("thankYou").innerHTML = "Thank you for using this service! Have a wonderful rest of your " + greeting + "!";
     }
 
-        //Display First and Last name in results div element
-        document.getElementById("firstLastNames").innerHTML = "Hello " + fName + " " + lName + ".";
-    
-        //Display the date in results div element
-        const d = new Date();
-        document.getElementById("displayTime").innerHTML = "Today's date is: " + d.toDateString()  + ".";
-    
 }
 
 function erase()
